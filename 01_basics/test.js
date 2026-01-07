5278368a2a3beaef025225add21248a4c63a980d
Maryum Sahar <151986872+maryumSahar-wevDeveloper@users.noreply.github.com>
2026-01-07 02:49:31 +0000

    datatypes cover

--- 01_basics/test.js
+++ 01_basics/test.js
@@ -1,1 +1,17 @@
-console.log("Hello Maryam")
+// console.log("Hello Maryam")
+const accountId = 100
+let name = "maryum"
+let isLoggedIn = false
+// number
+// string
+// boolean
+// bigIn
+// nuull is a standalone value and representation of an empty values
+// undefined
+// symbol is used to make component unique
+
+
+// object
+console.log(typeof null)
+// alert(accountId) Using node js not browser
+
