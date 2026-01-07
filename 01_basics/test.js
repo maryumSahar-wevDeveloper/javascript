fb1c81b7ed4effa51f35a5d0eb961478a5a261f2
Maryum Sahar <151986872+maryumSahar-wevDeveloper@users.noreply.github.com>
2026-01-07 02:59:11 +0000

    conversion covers

--- 01_basics/test.js
+++ 01_basics/test.js
@@ -1,7 +1,7 @@
 // console.log("Hello Maryam")
-const accountId = 100
-let name = "maryum"
-let isLoggedIn = false
+// const accountId = 100
+// let name = "maryum"
+// let isLoggedIn = false
 // number
 // string
 // boolean
@@ -12,6 +12,29 @@ let isLoggedIn = false
 
 
 // object
-console.log(typeof null)
+//console.log(typeof null)
 // alert(accountId) Using node js not browser
 
+// ++++++++++++++++++++Conversion and Oprations ++++++++++++++++++++++
+
+let score = false
+
+// console.log(typeof score);
+// console.log(typeof (score));
+
+let valusInNumber = Number(score)
+// console.log(typeof (valusInNumber));
+// console.log(valusInNumber);
+
+// "33" => 33
+// "33ess" => NaN
+// true => 1; false => 0
+
+let isLoggedIn = "maryam"
+let booleanLoggedIn = Boolean(isLoggedIn)
+console.log(booleanLoggedIn);
+
+let id = 123
+
+let idOf = String(id)
+console.log(typeof idOf);
