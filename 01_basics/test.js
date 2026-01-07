42b5a69bfd8433031e135dab138febe395188cad
Maryum Sahar <151986872+maryumSahar-wevDeveloper@users.noreply.github.com>
2026-01-07 03:16:48 +0000

    Opration cover

--- 01_basics/test.js
+++ 01_basics/test.js
@@ -32,9 +32,41 @@ let valusInNumber = Number(score)
 
 let isLoggedIn = "maryam"
 let booleanLoggedIn = Boolean(isLoggedIn)
-console.log(booleanLoggedIn);
+//console.log(booleanLoggedIn);
 
 let id = 123
 
 let idOf = String(id)
-console.log(typeof idOf);
+//console.log(typeof idOf);
+
+
+//+++++++ Oprations ++++++++
+
+let value = 3
+let negValue = -value
+
+//console.log(negValue);
+//console.log(2+2);
+//console.log(2-2);
+//console.log(2*2);
+//console.log(2**3);
+//console.log(2%3);
+let str1 = "Hello"
+let str2 = " Maryam"
+
+// console.log(str1 + str2);
+// console.log(`Hello my name is ${str1} and my raport is ${2+3}`);
+
+//console.log("1" + 2);
+//console.log(1 + "2");
+//console.log("1" + 2 + 3);
+//console.log((3 + 4) * 5 % 3);
+
+let x = 3
+let y = x++
+console.log("y:", y , "x:", x);
+
+a = 10
+b = ++a
+
+console.log("a:", a , "b:", b);
